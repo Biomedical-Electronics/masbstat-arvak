4cdf49bca1bea8f9ea0d6f137fd7f1d59ab92de2
rubencn <ruben1712@gmail.com>
2022-05-09 22:27:55 +0200

    Adding the code for initialization of the Power Management Unit (PMU)

---
+++ Core/Inc/components/PMU.h
@@ -0,0 +1,13 @@
+/*
+ * PMU.h
+ *
+ *  Created on: May 9, 2022
+ *      Author: Rubén Cuervo & Pere Pena
+ */
+
+#ifndef INC_COMPONENTS_PMU_H_
+#define INC_COMPONENTS_PMU_H_
+
+void initialize_PMU();
+
+#endif /* INC_COMPONENTS_PMU_H_ */
