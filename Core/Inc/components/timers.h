d0df78b469d0282f4cb486919be8233328963f9d
rubencn <ruben1712@gmail.com>
2022-05-31 15:51:12 +0200

    Introducing the getMeasure() function to correctly measure the variables at the desired rate

--- Core/Inc/components/timers.h
+++ Core/Inc/components/timers.h
@@ -6,5 +6,6 @@
 void initialize_timer(TIM_HandleTypeDef* htim3, double samplingPeriod);
 uint32_t getVoltage();
 uint32_t getCurrent();
+uint8_t getMeasure();
 
 #endif /* INC_COMPONENTS_TIMERS_H_ */
