d85df3e0e1a163e2a9d834c8bfe151cc2cb93e3d
Pere0 <ppenapuj7@alumnes.ub.edu>
2022-06-08 12:25:45 +0200

    Fix chronoamperometry.c file

--- Core/Src/components/chronoamperometry.c
+++ Core/Src/components/chronoamperometry.c
@@ -10,16 +10,18 @@
 #include "main.h"
 #include "components/masb_comm_s.h"
 #include "components/mcp4725_driver.h"
+#include "components/formulas.h"
 
 extern MCP4725_Handle_T hdac;
 extern TIM_HandleTypeDef htim3;
+extern ADC_HandleTypeDef hadc1;
 struct CA_Configuration_S caConfiguration;
 double vReal;
 double iReal;
-uint8_t cycle;
+uint32_t cycle;
 struct Data_S data;
 
-void cyclic_volt(){
+void chronoamp(){
 
 	caConfiguration = MASB_COMM_S_getCaConfiguration();
 
@@ -28,22 +30,42 @@ void cyclic_volt(){
 	HAL_GPIO_WritePin(RELAY_GPIO_Port, RELAY_Pin, GPIO_PIN_SET);
 
 	initialize_timer(&htim3, caConfiguration.samplingPeriodMs);
-	cycle = 1;
 	while (1){
-		vReal=calculateVrefVoltage(getVoltage());
-		iReal=calculateIcellCurrent(getCurrent());
+			if (getMeasure()){
+				vReal=calculateVrefVoltage(getVoltage());
+				iReal=calculateIcellCurrent(getCurrent());
 
-		data.point=cycle;
-		data.timeMs=cycle*caConfiguration.samplingPeriodMs;
-		data.voltage=vReal;
-		data.current=iReal;
+				data.point=1;
+				data.timeMs=0;
+				data.voltage=vReal;
+				data.current=iReal;
 
-		MASB_COMM_S_sendData(data);
-		cycle++;
+				MASB_COMM_S_sendData(data);
+				break;
+			}
+	}
 
-		if (data.timeMs>=caConfiguration.measurementTime){
-			break;
+	cycle=1;
+
+	while (1){
+		if (getMeasure()){
+			vReal=calculateVrefVoltage(getVoltage());
+			iReal=calculateIcellCurrent(getCurrent());
+
+			data.point=cycle+1;
+			data.timeMs=cycle*caConfiguration.samplingPeriodMs;
+			data.voltage=vReal;
+			data.current=iReal;
+
+			MASB_COMM_S_sendData(data);
+			cycle++;
+
+			if (data.timeMs>=caConfiguration.measurementTime*1000){
+				break;
+			}
 		}
 	}
+	HAL_TIM_Base_Stop_IT(&htim3);
+	HAL_ADC_Stop(&hadc1);
 	HAL_GPIO_WritePin(RELAY_GPIO_Port, RELAY_Pin, GPIO_PIN_RESET);
 }
