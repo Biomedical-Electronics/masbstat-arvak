fa483179bd43825e2e50e3a528134e8d721537a4
Pere0 <ppenapuj7@alumnes.ub.edu>
2022-06-08 15:54:34 +0200

    Adding a simple control flow for the stm

--- Core/Inc/components/masb_comm_s.h
+++ Core/Inc/components/masb_comm_s.h
@@ -9,6 +9,10 @@
 #define START_CA_MEAS		0x02
 #define STOP_MEAS			0x03
 
+#define CV					0x01
+#define CA					0x02
+#define IDLE				0x03
+
 #ifndef TRUE
     #define TRUE				1
 #endif
--- Core/Src/components/stm32main.c
+++ Core/Src/components/stm32main.c
@@ -1,12 +1,45 @@
 #include "components/stm32main.h"
 #include "components/masb_comm_s.h"
 #include "components/mcp4725_driver.h"
+#include "components/PMU.h"
+#include "components/chronoamperometry.h"
+#include "components/cyclic_voltammetry.h"
+
+uint8_t state;
 
 MCP4725_Handle_T hdac;
 void setup(struct Handles_S *handles) {
 	hdac = MCP4725_Init();
+	initialize_PMU();
 }
 
 void loop(void) {
-
+	if (MASB_COMM_S_dataReceived()) {
+		switch(MASB_COMM_S_command()) {
+			case START_CV_MEAS:
+				state=CV;
+				break;
+			case START_CA_MEAS:
+				state=CA;
+				break;
+			case STOP_MEAS:
+				state=IDLE;
+				break;
+			default:
+				break;
+		}
+	} else{
+		switch(state){
+			case CV:
+				cyclic_volt();
+				state=IDLE;
+				break;
+			case CA:
+				chronoamp();
+				state=IDLE;
+				break;
+			default:
+				break;
+		}
+	}
 }
